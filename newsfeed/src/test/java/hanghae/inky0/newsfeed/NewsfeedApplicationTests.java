5d8be8ca059725b007c88828e90873bb08371144
DESKTOP-BJ8G1LU\nikph <2020113392@dgu.ac.kr>
2024-01-26 13:05:14 +0900

    chore:init

---
+++ newsfeed/src/test/java/hanghae/inky0/newsfeed/NewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package hanghae.inky0.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
